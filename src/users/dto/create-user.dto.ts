4dfc7a3910c4d67ff97e7cccc3fdfba5c4510dfa
Wichy <wichy8780@gmail.com>
2025-04-14 17:03:37 -0400

    feat: validation of create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,43 @@
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  IsInt,
+  MinLength,
+  MaxLength,
+  IsPhoneNumber,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
 export class CreateUserDto {
-  id: number;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   userName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.trim().toLowerCase() : value,
+  )
   email: string;
+
+  @IsPhoneNumber('CU')
+  @IsNotEmpty()
   phone: string;
+
+  @IsInt()
+  @IsNotEmpty()
   roleId: number;
-  passsword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
 }
